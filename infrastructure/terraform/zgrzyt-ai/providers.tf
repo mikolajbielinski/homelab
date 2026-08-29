5729630292cdaab182174130074541942c47ab38
Mikolaj Bielinski <mikolaj.bielinski10@gmail.com>
2026-08-29 22:36:24 +0200

    feat: Terraform lock files moved to s3 for security. Changed aws secret to main zgrzy user

--- infrastructure/terraform/zgrzyt-ai/providers.tf
+++ infrastructure/terraform/zgrzyt-ai/providers.tf
@@ -1,4 +1,14 @@
 terraform {
+  required_version = ">= 1.10"
+
+  backend "s3" {
+    bucket       = "zgrzyt-ai-tfstate"
+    key          = "zgrzyt-ai/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
