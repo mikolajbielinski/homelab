4665f2621b7634d18414ae1e3fbe960fdad95e88
Mikolaj Bielinski <mikolaj.bielinski10@gmail.com>
2026-08-30 19:47:57 +0200

    feat: Remove old IAM users - now we have per component users only

--- infrastructure/terraform/zgrzyt-ai/iam.tf
+++ infrastructure/terraform/zgrzyt-ai/iam.tf
@@ -1,31 +1,3 @@
-resource "aws_iam_user" "zgrzyt" {
-  name = "zgrzyt-ai"
-}
-
-resource "aws_iam_user_policy" "zgrzyt_s3" {
-  name = "zgrzyt-ai-s3-access"
-  user = aws_iam_user.zgrzyt.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket",
-          "s3:DeleteObject"
-        ]
-        Resource = [
-          aws_s3_bucket.zgrzyt.arn,
-          "${aws_s3_bucket.zgrzyt.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
 resource "aws_iam_user" "orchestrator" {
   name = "zgrzyt-orchestrator"
 }
