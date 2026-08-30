f7ae9b0b352cc2ad29850540fe0af0e781047dd6
Mikolaj Bielinski <mikolaj.bielinski10@gmail.com>
2026-08-30 19:20:58 +0200

    feat: Spliat IAM users into smaller pieces - user per component.

--- infrastructure/terraform/zgrzyt-ai/iam.tf
+++ infrastructure/terraform/zgrzyt-ai/iam.tf
@@ -68,3 +68,98 @@ resource "aws_iam_user_policy" "orchestrator" {
     ]
   })
 }
+
+resource "aws_iam_user" "reconciler" {
+  name = "zgrzyt-reconciler"
+}
+
+resource "aws_iam_user_policy" "reconciler" {
+  name = "zgrzyt-reconciler-access"
+  user = aws_iam_user.reconciler.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = [aws_s3_bucket.zgrzyt.arn]
+        Condition = {
+          StringLike = {
+            "s3:prefix" = ["mp3/*"]
+          }
+        }
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:PutObject", "s3:AbortMultipartUpload"]
+        Resource = ["${aws_s3_bucket.zgrzyt.arn}/mp3/*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user" "embed" {
+  name = "zgrzyt-embed"
+}
+
+resource "aws_iam_user_policy" "embed" {
+  name = "zgrzyt-embed-access"
+  user = aws_iam_user.embed.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = [aws_s3_bucket.zgrzyt.arn]
+        Condition = {
+          StringLike = {
+            "s3:prefix" = ["transcripts/labeled/*"]
+          }
+        }
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject"]
+        Resource = ["${aws_s3_bucket.zgrzyt.arn}/transcripts/labeled/*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user" "speakers" {
+  name = "zgrzyt-speakers"
+}
+
+resource "aws_iam_user_policy" "speakers" {
+  name = "zgrzyt-speakers-access"
+  user = aws_iam_user.speakers.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = [aws_s3_bucket.zgrzyt.arn]
+        Condition = {
+          StringLike = {
+            "s3:prefix" = ["transcripts/raw/*", "transcripts/labeled/*"]
+          }
+        }
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject"]
+        Resource = ["${aws_s3_bucket.zgrzyt.arn}/transcripts/raw/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:PutObject"]
+        Resource = ["${aws_s3_bucket.zgrzyt.arn}/transcripts/labeled/*"]
+      }
+    ]
+  })
+}
